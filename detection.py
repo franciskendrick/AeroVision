05630900dde89604f6e4b82a00dd31bf5f949153
Francis Kendrick <franciskendrick1027@gmail.com>
2025-04-09 17:12:54 +0800

    Extracted keypoint values

--- detection.py
+++ detection.py
@@ -24,6 +24,15 @@ def draw_landmarks(image, results):
     # mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
 
 
+def extract_keypoints(results):
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten()
+    # lefthand = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+    # righthand = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+
+    return np.concatenate(pose)
+    # return np.concatenate([pose, lefthand, righthand])
+
+
 cap = cv2.VideoCapture(0)
 with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
     while cap.isOpened():
