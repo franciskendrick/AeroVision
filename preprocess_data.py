1799e606e7584a70a3369e788da9c88d50aab7bd
Francis Kendrick <franciskendrick1027@gmail.com>
2025-04-09 22:51:44 +0800

    Preprocessed data

---
+++ preprocess_data.py
@@ -0,0 +1,38 @@
+from sklearn.model_selection import train_test_split
+# from tensorflow.python.keras.utils import to_categorical
+# from keras.utils import to_categorical
+from keras._tf_keras.keras.utils import to_categorical
+import numpy as np
+import os
+
+DATA_PATH = os.path.join("MP_Data")
+actions = np.array(["straight_ahead", "turn_left", "turn_right"])
+no_sequences = 30  # 30 videos worth of data
+sequence_length = 30  # 30 frames in length
+
+label_map = {label:num for num, label in enumerate(actions)}
+
+#
+sequences, labels = [], []
+for action in actions:
+    for sequence in np.array(os.listdir(os.path.join(DATA_PATH, action))):
+        window = []
+        for frame_num in range(sequence_length):
+            res = np.load(os.path.join(DATA_PATH, action, "{}.npy".format(frame_num)))
+            window.append(res)
+        sequences.append(window)
+        labels.append(label_map[action])
+
+# print(np.array(sequences).shape)  # 90, 30, 132) or 90 videos, 30 frames per video, 132 keypoints per frame
+# print(np.array(labels).shape)  # (90,)
+
+#
+x = np.array(sequences)
+y = to_categorical(labels).astype(int)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.05)
+
+# print(x_train.shape)  # (85, 30, 132)
+# print(x_test.shape)  # (5, 30, 132)
+# print(y_train.shape)  # (85, 3)
+# print(y_test.shape)  # (5, 3)
--- training.py
+++ training.py
@@ -1,8 +1,6 @@
 import cv2
 import numpy as np
 import os
-from matplotlib import pyplot as plt
-import time
 import mediapipe as mp
 
 # Setup folder/s for data collection
