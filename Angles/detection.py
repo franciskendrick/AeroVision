32043b429d0fa4fea5a8d9f8f0e7ba5062fc8fdc
Francis Kendrick <franciskendrick1027@gmail.com>
2025-05-26 21:35:06 +0800

    Added 12, 14, 24, right shoulder angle detection

--- Angles/detection.py
+++ Angles/detection.py
@@ -3,18 +3,16 @@ import mediapipe as mp
 import math
 
 
-def calculate_angle(a, b, c):
-    # Vectors BA and BC
+def calculate_angle(a, b, c, direction="right"):
     ba = [a[0] - b[0], a[1] - b[1]]
     bc = [c[0] - b[0], c[1] - b[1]]
-
-    # Compute the angle between the vectors using atan2
     angle_rad = math.atan2(ba[1], ba[0]) - math.atan2(bc[1], bc[0])
     angle_deg = math.degrees(angle_rad) % 360
-
-    flipped_angle = (180 - angle_deg) % 360
-
-    return round(flipped_angle, 2)
+    if direction == "right":
+        flipped_angle = (180 - angle_deg) % 360
+    elif direction == "left":
+        flipped_angle = (angle_deg + 180) % 360
+    return round(flipped_angle)
 
 
 mp_drawing = mp.solutions.drawing_utils
@@ -35,20 +33,29 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
         if results.pose_landmarks:
             mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
 
-            # Get landmarks 11, 13, 23
             lm = results.pose_landmarks.landmark
             h, w, _ = image.shape
 
+            # Left shoulder angle (11, 13, 23)
             a = (lm[11].x * w, lm[11].y * h)
             b = (lm[13].x * w, lm[13].y * h)
             c = (lm[23].x * w, lm[23].y * h)
+            angle_left = calculate_angle(a, b, c)
+            print("Left:", angle_left)
 
-            angle = calculate_angle(a, b, c)
-            print(angle)
-
-            # Display angle below point 11
             cx, cy = int(a[0]), int(a[1])
-            cv2.putText(image, str(angle), (cx, cy + 30), cv2.FONT_HERSHEY_SIMPLEX,
+            cv2.putText(image, str(angle_left), (cx, cy + 30), cv2.FONT_HERSHEY_SIMPLEX,
+                        1, (255, 255, 255), 2, cv2.LINE_AA)
+
+            # Right shoulder angle (12, 14, 24)
+            a = (lm[12].x * w, lm[12].y * h)
+            b = (lm[14].x * w, lm[14].y * h)
+            c = (lm[24].x * w, lm[24].y * h)
+            angle_right = calculate_angle(a, b, c, "left")
+            print("Right:", angle_right)
+
+            cx_r, cy_r = int(a[0]), int(a[1])
+            cv2.putText(image, str(angle_right), (cx_r, cy_r + 30), cv2.FONT_HERSHEY_SIMPLEX,
                         1, (255, 255, 255), 2, cv2.LINE_AA)
 
         cv2.imshow('Pose Landmarks', image)
